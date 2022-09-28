56cdea528788d17e04ad7dfbca3fd0294937dd7d
savaged <savagedavid@gmail.com>
2022-09-28 08:51:13 +0100

    doco

--- WebApplication1/Models/User.cs
+++ WebApplication1/Models/User.cs
@@ -1,5 +1,12 @@
 ﻿namespace WebApplication1.Models
 {
+    /// <summary>
+    /// CREATE TABLE "User" (
+    ///     "Id"	INTEGER,
+    ///     "Fullname"	TEXT,
+    ///     PRIMARY KEY("Id" AUTOINCREMENT)
+    /// );
+    /// </summary>
     public class User : BaseModel
     {
         public string? Fullname { get; set; }
